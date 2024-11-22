eb1add08eeaea3447d2246839bd0967277d5a31c
kimyou1102 <yoosk5485@naver.com>
2024-11-22 21:12:40 +0900

    test: 컨트럴러가 담당하지 않는 기능에 대한 테스트 제거

--- __tests__/ControllerTest.js
+++ __tests__/ControllerTest.js
@@ -16,35 +16,7 @@ describe('컨트롤러 클래스 테스트', () => {
 
     const controller = new Controller();
 
-    const arr = INPUT.map((name) => new Car(name));
+    const arr = INPUT.map((name) => new Car(name, 0));
     expect(controller.createCars(INPUT)).toEqual(arr);
-  });
-
-  test('랜덤 숫자가 4이상이면 자동차를 전진시킬 수 있다.', () => {
-    const MOVING_FORWARD = 4;
-
-    mockRandoms([MOVING_FORWARD]);
-
-    const controller = new Controller();
-    expect(controller.canMoveForward()).toBe(true);
-  });
-
-  test('랜덤 숫자가 4미만이면 자동차를 전진시킬 수 없다.', () => {
-    const STOP = 3;
-
-    mockRandoms([STOP]);
-
-    const controller = new Controller();
-
-    expect(controller.canMoveForward()).toBe(false);
-  });
-
-  test('우승자를 찾는다', () => {
-    const CAR1 = new Car('pobi', 5);
-    const CAR2 = new Car('woni', 4);
-    const CARS = [CAR1, CAR2];
-    const controller = new Controller();
-
-    expect(controller.findWinners(CARS)).toEqual(['pobi']);
   });
 });
