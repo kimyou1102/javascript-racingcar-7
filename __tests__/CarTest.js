3808b11d051f87c79b7655227f3b7f89e6ca6159
kimyou1102 <yoosk5485@naver.com>
2024-11-22 15:50:22 +0900

    test: 자동차 클래스 테스트 작성

---
+++ __tests__/CarTest.js
@@ -0,0 +1,21 @@
+import { Car } from '../src/model/Car';
+
+describe('자동차 클래스 테스트', () => {
+  test('이름과 전진 정도를 가질 수 있다.', () => {
+    const NAME = 'pobi';
+
+    const car = new Car(NAME);
+
+    expect(car.getName()).toBe('pobi');
+    expect(car.getStep()).toBe(0);
+  });
+
+  test('자동차는 전진할 수 있다', () => {
+    const NAME = 'pobi';
+
+    const car = new Car(NAME);
+    car.move();
+
+    expect(car.getStep()).toBe(1);
+  });
+});
